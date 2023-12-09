be097614eb6c8d1c3d9de7e007a6e0e40667695b
kseniia_butko <apolloisggg@gmail.com>
2023-12-09 19:29:07 +0200

    Day 9 + remove puzzle inputs

---
+++ src/main/scala/Day9.scala
@@ -0,0 +1,32 @@
+object Day9 {
+  def main(args: Array[String]): Unit = {
+    val input = Util.readLines("day9.txt")
+    println(day9_1(input))
+  }
+
+  def parse(input: List[String]): List[List[Int]] = {
+    input.map(i => i.split(" ").map(_.toInt).toList)
+  }
+
+  def day9_1(input: List[String]): Int = {
+    val data = parse(input)
+
+    def loop(matrix: List[List[Int]], current: List[Int]): Int = {
+      if (current.forall(_ == 0)) matrix.foldLeft(0) {
+        (cur, row) =>
+          // cur + row.last // part 1
+          row.head - cur  // part 2
+      }
+      else {
+        val next = current.zip(current.tail).map {
+          case (x, x_1) => x_1 - x
+        }
+        loop(current :: matrix, next)
+      }
+    }
+
+    data.map {
+      record => loop(List.empty[List[Int]], record)
+    }.sum
+  }
+}
