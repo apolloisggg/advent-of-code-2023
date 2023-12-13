d2ac148fa1eca5f2d4347d976ded46e06c3dc3fd
kseniia_butko <apolloisggg@gmail.com>
2023-12-13 23:03:53 +0200

    Day 13: not finished

---
+++ src/main/scala/Day13.scala
@@ -0,0 +1,30 @@
+object Day13 {
+  def main(args: Array[String]): Unit = {
+    val input = Util.readLines("day13.txt")
+    parse(input).foreach(println)
+  }
+
+  def parse(input: List[String]): Array[List[String]] = {
+    input.mkString("\n").split("\n\n").map(x => x.split("\n").toList)
+  }
+
+  def verticalLine(list: List[String]): Option[Int] = {
+    val size = list.size
+
+    val rotated = (0 until size).map { index =>
+      list.foldLeft("")((l, s) => l(index) + s)
+    }
+
+    ???
+  }
+
+  def horizontalLine(list: List[String]): Int = {
+    list
+  }
+
+  def part1(input: List[String]): Int = {
+    parse(input).map { image =>
+
+    }
+  }
+}
