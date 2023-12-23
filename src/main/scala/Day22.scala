314c41d35ef8b5a246293825ddb7f136bcb80f21
kseniia_butko <apolloisggg@gmail.com>
2023-12-23 22:13:19 +0200

    Day 22: draft

--- src/main/scala/Day22.scala
+++ src/main/scala/Day22.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object Day22 {
   def main(args: Array[String]): Unit = {
     val input = Util.readLines("day22.txt")
@@ -8,20 +10,41 @@ object Day22 {
 
   def parse(input: List[String]): List[Brick] = {
     input.map {
-      case s"$x1,$y1,$z1~$x2,$y2,$z2" =>
-        Brick(
-          (x1.toInt to x2.toInt).toList,
-          (y1.toInt to y2.toInt).toList,
-          (z1.toInt to z2.toInt).toList
+        case s"$x1,$y1,$z1~$x2,$y2,$z2" =>
+          Brick(
+            (x1.toInt to x2.toInt).toList,
+            (y1.toInt to y2.toInt).toList,
+            (z1.toInt to z2.toInt).toList
         )
     }
   }
 
   def part1(input: List[String]): Int = {
-    val bricks = parse(input)
 
-    bricks.sortBy(- _.z.max).foreach(println)
+    val bricks = parse(input).sortBy(_.z.min)
 
-    0
+    @tailrec
+    def loop(bricks: List[Brick], processedBricks: List[Int], acc: Int): Int = bricks match
+      case Nil =>
+        println(processedBricks)
+        acc
+      case brick :: tail =>
+        val xy = for {
+          x <- brick.x
+          y <- brick.y
+        } yield 10 * y + x
+
+        val xyLayer = processedBricks
+          .filter(brick => xy.contains(brick % 100))
+
+        xyLayer match
+          case Nil => loop(tail, xy.map(_ + 100) ::: processedBricks, acc)
+          case _ =>
+
+            val zLayer = xyLayer.max / 100 + brick.z.length
+
+            loop(tail, xy.map(_ + zLayer * 100) ::: processedBricks, acc)
+
+    loop(bricks, List.empty, 0)
   }
 }
